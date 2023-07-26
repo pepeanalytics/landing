a943f7376eb9a181300730c50e2b84f763f24037
Dying <47161228+dyingg@users.noreply.github.com>
2023-07-27 04:40:19 +0530

    fix: env variable

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,3 @@
 export const WALLET_CONNECT_ID = "dfd5e097a8c59c5f38ac4dc84d86c160";
 
-export const NETWORK = process.env.NETWORK?.toUpperCase() || "POLYGON";
+export const NETWORK = process.env.REACT_APP_NETWORK?.toUpperCase() || "ETH";
